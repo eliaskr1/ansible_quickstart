f15d4049edc00fbfa76893edc9c6cec8f54489a2
Elias Kroon <elias.kroon19@gmail.com>
2024-08-27 14:01:10 +0200

    second commit

---
+++ page/canvas.js
@@ -0,0 +1,5 @@
+import { Application } from "https://esm.sh/@splinetool/runtime";
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+		app.load('https://prod.spline.design/IIDgiku-GASS-44V/scene.splinecode').then(() => {
+});
